ff542aefcf3081c575042d8838c2351c1e6e6e0f
unknown <aurelio>
2023-07-12 22:47:23 -0600

    agregando primer archivo

---
+++ Creacion_tablas.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE PruebaSQL
+
+USE PruebaSQL
+GO
+
+CREATE TABLE Personas(
+    id int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(50)
+)
+
+CREATE TABLE Proyectos(
+    id int PRIMARY KEY IDENTITY(1,1),
+    NombreProyecto varchar(50)
+)
+
+--Agregar otros campos
+ALTER TABLE Personas ADD Apellido varchar(50)
+
+--
+INSERT INTO Proyectos VALUES('Proyecto 1')
+SELECT *  FROM Proyectos
+
+--ELIMINAR TABLA
+DROP TABLE Proyectos
+
+--ELIMINAR DATOS TABLA 
+TRUNCATE TABLE Proyectos --Elimina la tabla de forma total. No deja logs ni dispara triggers
+
+DELETE FROM Proyectos --Elimina linea por linea dejando logs y no resetea los identificadores (no es muy eficiente)
+
+
